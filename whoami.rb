2853245460aee0dcb639f8562449c528833e8385
RobertChang <robertchang0722@gmail.com>
2022-09-04 23:11:32 +0800

    add show environment variable feature

--- whoami.rb
+++ whoami.rb
@@ -7,5 +7,5 @@ get '/' do
   ip_address = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
   hostname = Socket.gethostname
 
-  ["容器名稱：#{hostname}", "容器的 IP 位置：#{ip_address}"].join("<br>")
+  ["容器名稱：#{hostname}", "容器的 IP 位置：#{ip_address}", "環境變數 AUTHOR 是：#{ENV.fetch('AUTHOR', nil)}"].join("<br>")
 end
